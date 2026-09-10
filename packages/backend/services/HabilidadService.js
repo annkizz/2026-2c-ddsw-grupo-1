b0683af3d4fc733a4e6deb1a61fdf0a4d755dcd5
Lara Puelman <lpuelman@frba.utn.edu.ar>
2026-09-10 09:02:21 -0300

    Corrección de mensaje habilidad

--- packages/backend/services/HabilidadService.js
+++ packages/backend/services/HabilidadService.js
@@ -15,7 +15,7 @@ export class HabilidadService {
       const habilidad = new HabilidadProyecto(habilidadNueva.titulo);
       return this.habilidadRepository.save(habilidad);
     }
-    throw new ConflictError("el producto ya existe!!");
+    throw new ConflictError("La habilidad ya existe!!");
   };
 
   obtenerTodos() {
